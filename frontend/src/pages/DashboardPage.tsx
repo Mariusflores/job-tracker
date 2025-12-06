a5603cf63d5a337ccafa751541f217833e2a5fde
Marius Flores Dalseng <mfloresdal@gmail.com>
2025-12-06 18:02:37 +0100

    Fixed minor warning

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -41,7 +41,7 @@ export function DashboardPage() {
     console.log("Applications fetched: ", apps);
     return (
         <div className="min-h-screen bg-gray-50  flex justify-center">
-            <div className="bg-white shadow p-4 mt-20 space-y-4 p-6 md:p-10 w-full max-w-4xl mx-auto rounded-lg">
+            <div className="bg-white shadow mt-20 space-y-4 p-6 md:p-10 w-full max-w-4xl mx-auto rounded-lg">
                 <div className={"flex flex-row justify-between"}>
                     <h2 className="text-3xl text-black font-semibold mb-4">Dashboard</h2>
                     <button onClick={openModal}
