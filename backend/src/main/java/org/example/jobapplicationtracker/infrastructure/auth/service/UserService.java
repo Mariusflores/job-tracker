b1d31973c5e5cc083cb57e442d352c84fbbb1d38
Marius Flores Dalseng <mfloresdal@gmail.com>
2026-02-12 21:19:30 +0100

    removed unused import

--- backend/src/main/java/org/example/jobapplicationtracker/infrastructure/auth/service/UserService.java
+++ backend/src/main/java/org/example/jobapplicationtracker/infrastructure/auth/service/UserService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.jobapplicationtracker.infrastructure.auth.context.CurrentUserProvider;
 import org.example.jobapplicationtracker.infrastructure.auth.model.User;
-import org.example.jobapplicationtracker.infrastructure.auth.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
-    private final UserRepository userRepository;
     private final CurrentUserProvider currentUserProvider;
 
     @Transactional
