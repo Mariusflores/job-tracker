ee5a6abf6397b876a68692697feddbaa9623ee08
Marius Flores Dalseng <mfloresdal@gmail.com>
2026-03-05 20:16:35 +0100

    Updated migration script

--- backend/src/main/resources/db/migration/V4__fix_action_type_enum.sql
+++ backend/src/main/resources/db/migration/V4__fix_action_type_enum.sql
@@ -1,3 +1,5 @@
--- V5__fix_action_type_enum.sql
+-- V4__fix_action_type_enum.sql
+
 ALTER TABLE t_idempotency_record
-ALTER COLUMN action_type TYPE varchar(255);
+ALTER COLUMN action_type TYPE VARCHAR(50)
+USING action_type::VARCHAR;
